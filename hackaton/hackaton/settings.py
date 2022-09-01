8c69d1a301e3be3877161898806d1bb69528c262
ALEJO-29 <alejandro123aut@gmail.com>
2022-09-01 12:41:04 -0500

    carga de app userApp

--- hackaton/hackaton/settings.py
+++ hackaton/hackaton/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'bootstrap5',
     'hackatonApp',
     'adminApp',
+    'userApp',
 ]
 
 MIDDLEWARE = [
---
+++ hackaton/userApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def loginUser(request):
+    return render(request, 'login.html')
