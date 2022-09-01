236cee3cadfab1298e0fd2df04539414f58d5e69
Sebastian Gonzalez <sebas2rus@gmail.com>
2022-09-01 15:36:47 -0500

    Añadiendo categoria

--- hackaton/adminApp/admin.py
+++ hackaton/adminApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from adminApp.models import Productos
+from adminApp.models import Productos, Categoria
 # Register your models here.
 
 admin.site.register(Productos)
+admin.site.register(Categoria)
--- hackaton/adminApp/models.py
+++ hackaton/adminApp/models.py
@@ -3,15 +3,30 @@ from django.db import models
 
 # Create your models here.
 
+
+class Categoria(models.Model):
+    nombre = models.CharField( max_length=50)
+    
+    
+    def __str__(self):
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        return self.nombre
+        
+        
 class Productos(models.Model):
     nombre = models.CharField( max_length=50)
     imagen = models.ImageField(upload_to="admin", blank=True, null=True)
     descripcion = models.CharField( max_length=300)
     precio = models.FloatField()    
-    unidades = models.IntegerField()
+    unidades_disponibles = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=None)
+                                  
     
     def __str__(self):
         verbose_name = "Producto"
         verbose_name_plural = "Productos"
+        info = "%s $%s Unidades disponibles: %s" % (self.nombre,self.precio,self.unidades_disponibles)
+        return info
     
         
