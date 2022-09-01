6ffe36be1ef05928cd2ef6277696c54f8ef5fea5
Sebastian Gonzalez <sebas2rus@gmail.com>
2022-09-01 12:42:02 -0500

    Create model adminApp

--- hackaton/adminApp/models.py
+++ hackaton/adminApp/models.py
@@ -1,3 +1,17 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Productos(models.Model):
+    nombre = models.CharField( max_length=50)
+    imagen = models.ImageField(upload_to="admin", blank=True, null=True)
+    descripcion = models.CharField( max_length=300)
+    precio = models.FloatField()    
+    unidades = models.IntegerField()
+    
+    def __str__(self):
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+    
+        
